31ab3a3ad4fe5b281fde6584b10f44985d1e14d4
Eve-LynBremner <emvanschoor@gmail.com>
2026-02-22 20:53:27 +0000

    added drop down list for licenses

--- readme_questions.py
+++ readme_questions.py
@@ -1,9 +1,38 @@
 questions = [
-    {"type": "input", "name": "title", "message": "What is your project title?"},
-    {"type": "input", "name": "description", "message": "Describe your project."},
-    {"type": "input", "name": "installation", "message": "Provide installation instructions."},
-    {"type": "input", "name": "usage", "message": "Provide usage information."},
-    {"type": "input", "name": "license", "message": "Select a license from the list:"},
-    {"type": "input", "name": "author", "message": "What is the author's name?"},
-    {"type": "input", "name": "contact", "message": "Provide contact information"},
+    {
+        "type": "input", 
+        "name": "title", 
+        "message": "What is your project title?"
+     },
+    {
+        "type": "input", 
+        "name": "description", 
+        "message": "Describe your project."
+     },
+    {
+        "type": "input", 
+        "name": "installation", 
+        "message": "Provide installation instructions."
+     },
+    {
+        "type": "input", 
+        "name": "usage", 
+        "message": "Provide usage information."
+     },
+    {
+        "type": "list", 
+        "name": "license", 
+        "message": "Select a license from the list:", 
+        "choices" : ["MIT License", "Apache License 2.0", "GPLv3", "BSD 3-Clause", "No License"]
+        },
+    {
+        "type": "input", 
+        "name": "author", 
+        "message": "What is the author's name?"
+        },
+    {
+        "type": "input", 
+        "name": "contact", 
+        "message": "Provide contact information"
+        },
 ]
