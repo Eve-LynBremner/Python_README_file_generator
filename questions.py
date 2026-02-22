94dfdfffdf613ff7db49831e5e7073cb8580c60e
Eve-LynBremner <emvanschoor@gmail.com>
2026-02-22 17:34:23 +0000

    deleted files as created new ones with new names

--- questions.py
+++
@@ -1,9 +0,0 @@
-questions = [
-    {"type": "input", "name": "title", "message": "What is your project title?"},
-    {"type": "input", "name": "description", "message": "Describe your project."},
-    {"type": "input", "name": "installation", "message": "Provide installation instructions."},
-    {"type": "input", "name": "usage", "message": "Provide usage information."},
-    {"type": "input", "name": "license", "message": "Select a license from the list:"},
-    {"type": "input", "name": "author", "message": "What is the author's name?"},
-    {"type": "input", "name": "contact", "message": "Provide contact information"},
-]
